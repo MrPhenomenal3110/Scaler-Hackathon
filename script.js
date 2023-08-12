317aa32ec79b3cd92774ee2d5c6851089c52fc2b
Prem B. Shah <techie.prembshah@gmail.com>
2023-08-12 17:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+{/* <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+var typed = new Typed('#element', {
+    strings: ['Hii','I am Prem'],
+    typeSpeed: 50,
+  }); */}
